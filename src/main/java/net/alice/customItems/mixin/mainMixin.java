896768805a76d37579e4c4a36ed7213e3243d287
jate-koh <jate_ben@hotmail.com>
2022-12-30 18:36:09 +0700

    [feat] initial mixin configuration

--- src/main/java/net/alice/customItems/mixin/mainMixin.java
+++ src/main/java/net/alice/customItems/mixin/mainMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class mainMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
 		main.LOGGER.info("This line is printed by an example mod mixin!");
