c9154ea0edf0566c47b254b72a40bd3e64e7085f
jate-koh <jate_ben@hotmail.com>
2022-12-30 18:31:58 +0700

    [build] add dev client environment

---
+++ src/main/java/net/alice/customItems/mainClient.java
@@ -0,0 +1,9 @@
+package net.alice.customItems;
+
+import net.fabricmc.api.ClientModInitializer;
+public class mainClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
